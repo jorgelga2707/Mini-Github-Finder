f548edaba2d73c65c8b08fe19ca03dd59ba55e19
Jorge Garcia <jorge.garcia.a@uni.pe>
2023-01-31 06:39:45 -0500

    primerva version de mini github finder using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
 
-function App() {
+const App =() => {
+  const [search, setSearch] = useState("jorgelga2707");
+  const [user, setUser] = useState({user:"jorgelga2707",});
+  const fetchUser = async () => {
+    const response = await fetch(`https://api.github.com/users/${search}`);
+    const data = await response.json();
+    setUser(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Buscador de Usuarios</h1>
+        <div className="grid">
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            placeholder="ingrese el nombre de usuario"
+          />
+        </div>
+        <div className="btn-container">
+          <button className="btn-primary" onClick={fetchUser}>Buscar</button>
+        </div>
+        <article>
+          <div className="container data-info">
+            <img className="image-user" src={user.avatar_url} alt="avatar" />
+            <h4>username: {user.login}</h4>
+            <p>bio: {user.bio}</p>
+          </div>
+        </article>
+      </div>
     </div>
   );
 }
